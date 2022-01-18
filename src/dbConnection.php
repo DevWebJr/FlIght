3df6338f7bb269053197b15dfb1f7e3a24eb0687
Tito <50543732+DevWebJr@users.noreply.github.com>
2022-01-18 12:35:39 +0100

    update javascript and refactoring project

---
+++ src/dbConnection.php
@@ -0,0 +1,8 @@
+<?php
+
+$servername = "localhost:3308";
+$username = "root";
+$password = "";
+$dbname = "movies";
+
+$connection = mysqli_connect($servername, $username, $password, $dbname);
---
+++ src/flight.sql
@@ -0,0 +1,48 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3308
+-- Généré le : mar. 18 jan. 2022 à 11:31
+-- Version du serveur : 5.7.36
+-- Version de PHP : 7.4.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `flight`
+--
+DROP DATABASE IF EXISTS `flight`;
+CREATE DATABASE IF NOT EXISTS `flight` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `flight`;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `aircraft`
+--
+
+DROP TABLE IF EXISTS `aircraft`;
+CREATE TABLE IF NOT EXISTS `aircraft` (
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `icao24` varchar(255) DEFAULT NULL,
+  `callsign` varchar(255) DEFAULT NULL,
+  `latitude` float DEFAULT NULL,
+  `longitude` float DEFAULT NULL,
+  `estDepartureAirport` varchar(255) DEFAULT NULL,
+  `estArrivalAirport` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
