97e67783c3f4271501fa434a7bde8563770e5a24
Tito <50543732+DevWebJr@users.noreply.github.com>
2022-01-18 11:57:07 +0100

    customize website

--- script.js
+++ script.js
@@ -1,25 +1,34 @@
-const BEGIN = Math.floor(Date.now() / 1000 - 96 * 60 * 60); // Heure actuelle - 12h
-const END = Math.floor(Date.now() / 1000 - 95.5 * 60 * 60); // Heure actuelle - 11h30
-const URL_OPENSKY = "https://opensky-network.org/api/flights/all?begin=" + BEGIN + "&end=" + END;
-const WEATHER_APIKEY = '62c28e834631c3fec8ba0451d3573532';
+const BEGIN=Math.floor(Date.now() / 1000 - 96 * 60 * 60); // Heure actuelle - 96h
+const END=Math.floor(Date.now() / 1000 - 95.5 * 60 * 60); // Heure actuelle - 95h30
+const URL_OPENSKY="https://opensky-network.org/api/flights/all?begin=" + BEGIN + "&end=" + END;
+const WEATHER_APIKEY='62c28e834631c3fec8ba0451d3573532';
 
-let box = document.querySelector('.principal-box');
-let child = document.querySelector('.box-child');
+let box=document.querySelector('.box-container');
+let tab=document.querySelector('#tab');
 
 
 function displayFlights() {
     fetch(URL_OPENSKY).then(response =>
         response.json().then(data => {
             console.log(data);
-            let display = '<div class="container">';
+            let display = '<table id="tab" class="table table-hover"><thead><tr><th scope="col"><i class="fas fa-space-shuttle text-center text-primary m-5" title="icao24"></i></th><th scope="col"><i class="fab fa-battle-net text-center text-primary m-5" title="callsign"></i></th><th scope="col"><i class="fas fa-long-arrow-alt-right text-center text-success m-5" title="latitude"></i></th><th scope="col"><i class="fas fa-long-arrow-alt-up text-center text-success m-5" title="longitude"></i></th><th scope="col"><i class="fas fa-plane-departure text-center text-warning m-5" title="depart"></i></th><th scope="col"><i class="fas fa-plane-arrival text-center text-warning m-5" title="arrivée"></th><th scope="col"><i class="fas fa-search-location text-center text-info m-5" title="consulter"></th></tr></thead></table><tbody><tr class="table-hover">';
             for (let flight of data) {
-                display += `<div>
-                                        <i class=\"fas fa-plane-departure text-center text-primary m-5\" title="Départ"> ${flight.estDepartureAirport}</i>
-                                        <i class=\"fas fa-plane-arrival text-center text-warning m-5\" title="Arrivée"> ${flight.estArrivalAirport}</i>
-                                    </div>`
+                display += `   
+                            <th scope="row"> ${flight.icao24}</th> 
+                            <td class="text-center">${flight.callsign}</td> 
+                            <td class="text-center">${flight.latitude}</td> 
+                            <td class="text-center">${flight.longitude}</td> 
+                            <td class="text-center">${flight.estDepartureAirport}</td>
+                            <td class="text-center">${flight.estArrivalAirport}</td>
+                            <td class="text-center"><button type="button" class="btn btn-outline-info"><i class="fas fa-hand-pointer" title="details"></i></button></td>
+                        </tr> 
+                    </tbody> 
+                `;
             }
-            display += '</div>';
-            box.innerHTML = display;
-        })
+            display += '</table>';
+            tab.innerHTML = display;
+            })
     ).catch(err => console.log('Erreur : ' + err));
 }
+
+displayFlights();
